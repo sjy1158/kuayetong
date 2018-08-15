660521e80293c421547880a9e506f9b71712b332
sjy1158 <1124080755@qq.com>
2018-08-15 14:00:33 +0800

    sjy

--- src/pages/nearbyShops.vue
+++ src/pages/nearbyShops.vue
@@ -409,8 +409,10 @@
         this.$api.getMark(params).then((res)=>{
           _this.mark=res.user.mark
            if(_this.mark=='1'){
+              _this.$route.meta.keepAlive=false;
              _this.$router.push('/shareSome')
            }else{
+             _this.$route.meta.keepAlive=true;
              o.getCurrentPosition((status,result)=>{
                if(result&&result.position) {
                  _this.city = result.addressComponent.city;
