ea3c6bd608c85fd9a25ebd08f09ac67e6299092a
sjy1158 <1124080755@qq.com>
2018-11-15 17:37:49 +0800

    sjy

--- src/pages/activityProfile.vue
+++ src/pages/activityProfile.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="app" style="">
+    <div id="Loading" v-show="loading">
+      <div class="loader-inner ball-beat">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+
     <div class="title">
      {{dataobj.title}}
     </div>
@@ -27,7 +35,8 @@
               id:this.$route.query.id
             },
             dataobj:{},
-            str:''
+            str:'',
+            loading:true
           }
       },
       beforeCreate () {
@@ -49,6 +58,7 @@
           getActivity(params){
             const _this = this;
             this.$api.activityDetail(params).then((res)=>{
+                _this.loading = false;
                 res.activityNotice.time = _this.changeTime(res.activityNotice.time);
                 res.activityNotice.detalis = JSON.parse(res.activityNotice.detalis);
                 // alert(JSON.stringify(res.activityNotice.detalis));
@@ -135,4 +145,49 @@
     letter-spacing: 2px;
     line-height: 25px;
   }
+  /*loading加载*/
+  #Loading {
+    top:50%;
+    left:50%;
+    position: absolute;
+    -webkit-transform: translateY(-50%) translateX(-50%);
+    transform: translateY(-50%) translateX(-50%);
+    z-index:100;
+  }
+  @-webkit-keyframes ball-beat {
+    50% {
+      opacity: 0.2;
+      -webkit-transform: scale(0.75);
+      transform: scale(0.75); }
+
+    100% {
+      opacity: 1;
+      -webkit-transform: scale(1);
+      transform: scale(1); } }
+
+  @keyframes ball-beat {
+    50% {
+      opacity: 0.2;
+      -webkit-transform: scale(0.75);
+      transform: scale(0.75); }
+
+    100% {
+      opacity: 1;
+      -webkit-transform: scale(1);
+      transform: scale(1); } }
+
+  .ball-beat > div {
+    background-color: #F67419;
+    width: 15px;
+    height: 15px;
+    border-radius: 100% !important;
+    margin: 2px;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+    display: inline-block;
+    -webkit-animation: ball-beat 0.7s 0s infinite linear;
+    animation: ball-beat 0.7s 0s infinite linear; }
+  .ball-beat > div:nth-child(2n-1) {
+    -webkit-animation-delay: 0.35s !important;
+    animation-delay: 0.35s !important; }
 </style>
